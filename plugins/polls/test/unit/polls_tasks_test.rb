ce98d5ec760abf4efae257df50b308c2e0c1d125
SUGIMOTO Norimitsu <dictoss@live.jp>
2023-05-03 16:29:04 +0900

    add unittest template.

---
+++ plugins/polls/test/unit/polls_tasks_test.rb
@@ -0,0 +1,14 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+class PollsTaskSystemTest < ActiveSupport::TestCase
+  def setup
+  end
+
+  def test_version
+     puts "IN test_version()"
+  end
+
+  def test_print_settings
+     puts "IN test_print_settings()"
+  end
+end
