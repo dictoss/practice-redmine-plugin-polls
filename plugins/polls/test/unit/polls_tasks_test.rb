843070a0f5795bda78d2bd1ba09ee3b79a69684b
SUGIMOTO Norimitsu <dictoss@live.jp>
2023-05-10 23:18:25 +0900

    add more test.

--- plugins/polls/test/unit/polls_tasks_test.rb
+++ plugins/polls/test/unit/polls_tasks_test.rb
@@ -5,10 +5,14 @@ class PollsTaskSystemTest < ActiveSupport::TestCase
   end
 
   def test_version
-     puts "IN test_version()"
+    puts "IN test_version()"
+    Rake::Task["polls:system:version"].execute
+    Rake::Task["polls:system:version"].invoke
   end
 
   def test_print_settings
-     puts "IN test_print_settings()"
+    puts "IN test_print_settings()"
+    Rake::Task["polls:system:print_settings"].execute
+    Rake::Task["polls:system:print_settings"].invoke
   end
 end
